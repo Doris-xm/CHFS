2f5f91fd9f5b750641a6c95502eced0d3eef9069
Du Xinmin <114809783+Doris-xm@users.noreply.github.com>
2023-11-19 08:52:05 +0800

    fix 1c

--- src/distributed/client.cc
+++ src/distributed/client.cc
@@ -34,6 +34,10 @@ auto ChfsClient::mknode(FileType type, inode_id_t parent,
     if (res.is_err()) {
         return res.unwrap_error();
     }
+    auto ret_val = res.unwrap()->as<inode_id_t>();
+    if(ret_val == KInvalidInodeID){
+        return ChfsResult<inode_id_t>{ErrorType::AlreadyExist};
+    }
     return res.unwrap()->get().as<chfs::inode_id_t>();
 }
 
@@ -41,32 +45,55 @@ auto ChfsClient::mknode(FileType type, inode_id_t parent,
 auto ChfsClient::unlink(inode_id_t parent, std::string const &name)
     -> ChfsNullResult {
   // TODO: Implement this function.
-  UNIMPLEMENTED();
-  return KNullOk;
+    auto res = metadata_server_->call("unlink", parent, name);
+    if (res.is_err()) {
+        return res.unwrap_error();
+    }
+    return KNullOk;
 }
 
 // {Your code here}
 auto ChfsClient::lookup(inode_id_t parent, const std::string &name)
     -> ChfsResult<inode_id_t> {
   // TODO: Implement this function.
-  UNIMPLEMENTED();
-  return ChfsResult<inode_id_t>(0);
+    auto res = metadata_server_->call("lookup", parent, name);
+    if (res.is_err()) {
+        return res.unwrap_error();
+    }
+    auto ret_val = res.unwrap()->as<inode_id_t>();
+    if(ret_val == KInvalidInodeID){
+        return ChfsResult<inode_id_t>{ErrorType::NotExist};
+    }
+    return res.unwrap()->get().as<chfs::inode_id_t>();
 }
 
 // {Your code here}
 auto ChfsClient::readdir(inode_id_t id)
     -> ChfsResult<std::vector<std::pair<std::string, inode_id_t>>> {
   // TODO: Implement this function.
-  UNIMPLEMENTED();
-  return ChfsResult<std::vector<std::pair<std::string, inode_id_t>>>({});
+    auto res = metadata_server_->call("readdir", id);
+    if (res.is_err()) {
+        return res.unwrap_error();
+    }
+    auto ret_val = res.unwrap()->as<std::vector<std::pair<std::string, inode_id_t>>>();
+    if(ret_val.empty()){
+        return ChfsResult<std::vector<std::pair<std::string, inode_id_t>>>{ErrorType::NotExist};
+    }
+    return res.unwrap()->get().as<std::vector<std::pair<std::string, inode_id_t>>>();
 }
 
 // {Your code here}
 auto ChfsClient::get_type_attr(inode_id_t id)
     -> ChfsResult<std::pair<InodeType, FileAttr>> {
   // TODO: Implement this function.
-  UNIMPLEMENTED();
-  return ChfsResult<std::pair<InodeType, FileAttr>>({});
+    auto res = metadata_server_->call("get_type_attr", id);
+    if (res.is_err()) {
+        return res.unwrap_error();
+    }
+    auto ret_val = res.unwrap()->as<std::tuple<u64, u64, u64, u64, u8>>();
+    InodeType inodeType = (InodeType)std::get<4>(ret_val);
+    FileAttr fileAttr = {std::get<0>(ret_val), std::get<1>(ret_val), std::get<2>(ret_val), std::get<3>(ret_val)};
+    return ChfsResult<std::pair<InodeType, FileAttr>>{std::make_pair(inodeType, fileAttr)};
 }
 
 /**
@@ -87,6 +114,7 @@ auto ChfsClient::read_file(inode_id_t id, usize offset, usize size)
 
     std::vector<u8> read_res;
     auto curr_pos = offset % DiskBlockSize;
+    auto has_read = 0;
     for (auto i = start_pos; i < start_pos + num_block; ++i) {
         auto res = data_servers_[std::get<1>(block_info[i])]->call("read_data",
                                    std::get<0>(block_info[i]), curr_pos, DiskBlockSize - curr_pos,std::get<2>(block_info[i]));
@@ -94,7 +122,14 @@ auto ChfsClient::read_file(inode_id_t id, usize offset, usize size)
             return res.unwrap_error();
         }
         auto buffer = res.unwrap()->as<std::vector<u8>>();
-        read_res.insert(read_res.end(), buffer.begin(), buffer.end());
+        if (buffer.size() > size - has_read) {
+            read_res.insert(read_res.end(), buffer.begin(), buffer.begin() + size - has_read);
+            has_read += size - has_read;
+        }
+        else {
+            read_res.insert(read_res.end(), buffer.begin(), buffer.end());
+            has_read += DiskBlockSize - curr_pos;
+        }
         curr_pos = 0;
     }
     return ChfsResult<std::vector<u8>>(read_res);
@@ -137,10 +172,13 @@ auto ChfsClient::write_file(inode_id_t id, usize offset, std::vector<u8> data)
 
 // {Your code here}
 auto ChfsClient::free_file_block(inode_id_t id, block_id_t block_id,
-                                 mac_id_t mac_id) -> ChfsNullResult {
+                                     mac_id_t mac_id) -> ChfsNullResult {
   // TODO: Implement this function.
-  UNIMPLEMENTED();
-  return KNullOk;
+    auto free_call = metadata_server_->call("free_block",id,block_id,mac_id);
+    if(free_call.is_err()){
+        return ChfsNullResult {free_call.unwrap_error()};
+    }
+    return KNullOk;
 }
 
 } // namespace chfs
